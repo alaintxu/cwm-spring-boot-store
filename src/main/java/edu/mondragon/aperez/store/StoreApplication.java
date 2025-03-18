8ef73be6618a00902e3e12aee9dbc3d02bef2fd8
Alain PEREZ <aperez@mondragon.edu>
2025-03-18 11:25:29 +0000

    2x03 - Constructor Injection

--- src/main/java/edu/mondragon/aperez/store/StoreApplication.java
+++ src/main/java/edu/mondragon/aperez/store/StoreApplication.java
@@ -1,13 +1,17 @@
 package edu.mondragon.aperez.store;
 
-import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StoreApplication.class, args);
+		//SpringApplication.run(StoreApplication.class, args);
+		OrderService orderService = new OrderService(new StripePaymentService());
+		orderService.placeOrder();
+		OrderService orderService2 = new OrderService(new PayPalPaymentService());
+		orderService2.placeOrder();
 	}
 
 }
