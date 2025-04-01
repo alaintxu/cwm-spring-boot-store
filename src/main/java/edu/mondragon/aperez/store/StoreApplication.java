bbcaf7ff2352637cad684ece9548d493e81679c9
Alain PEREZ <aperez@mondragon.edu>
2025-04-01 12:39:22 +0000

    3x7.2 - Simplifying Code with Lombok

--- src/main/java/edu/mondragon/aperez/store/StoreApplication.java
+++ src/main/java/edu/mondragon/aperez/store/StoreApplication.java
@@ -2,13 +2,20 @@ package edu.mondragon.aperez.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import edu.mondragon.aperez.store.entities.User;
 
 
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StoreApplication.class, args);
+		//SpringApplication.run(StoreApplication.class, args);
+
+		User.builder()
+				.name("name")
+				.email("email")
+				.password("password")
+				.build();
 	}
 
 }
