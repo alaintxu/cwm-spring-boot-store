c12351f0d173f7a46d3cfaed8a62977a8c6421a7
Alain PEREZ <aperez@mondragon.edu>
2025-04-02 13:14:52 +0000

    3x8.2 - Using Repositories

--- src/main/java/edu/mondragon/aperez/store/StoreApplication.java
+++ src/main/java/edu/mondragon/aperez/store/StoreApplication.java
@@ -2,13 +2,35 @@ package edu.mondragon.aperez.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+// import edu.mondragon.aperez.store.entities.User;
+//import edu.mondragon.aperez.store.repositories.AddressRepository;
+// import edu.mondragon.aperez.store.repositories.UserRepository;
 
 
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StoreApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+		// UserRepository userRepository = context.getBean(UserRepository.class);
+		//AddressRepository addressRepository = (AddressRepository) context.getBean("AddressRepository");
+
+		// User user = User.builder()
+		// 	.name("John Doe")
+		// 	.email("john.doe@adibidea.eus")
+		// 	.password("pasahitza")
+		// 	.build();
+		//userRepository.save(user);
+
+		// User user = userRepository.findById(1L).orElseThrow();
+		// System.out.println(user.getName());
+
+		// userRepository.findAll().forEach(user -> {
+		// 	System.out.println(user.getName());
+		// });
+
+		// userRepository.deleteById(1L);
 	}
 
 }
